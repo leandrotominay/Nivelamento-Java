a4ce44c75e548df962071ee9b2f938dd077d22de
leandrotominay <ramosleandro53@gmail.com>
2020-06-10 14:24:25 -0300

    Nivelamento Java ex00 ex01 ex02

---
+++ src/ex01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ex01 {
+            /* Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+
+    int a, b ;
+
+
+
+
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor da soma: ");
+    a = sc.nextInt();
+        System.out.println("Digite o segundo valor da soma: ");
+    b = sc.nextInt();
+    int soma = a + b;
+        System.out.println(a + " + " + b + " = " + soma);
+
+
+             */
+}
